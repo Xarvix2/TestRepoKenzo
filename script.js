da6eff36c991d8a28d2bbff83a65500abba02e0d
Kenzo <kenzo.giardina@gmail.com>
2024-09-11 14:47:36 +0200

    test

---
+++ script.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("btn")
+
+
+const handleClick = () => {
+    console.log("cliquer...")
+}
+
+btn.addEventListener("click",function() {
+    console.log("cliquer !")
+})
+
+
+console.log("test ...",btn)
